8715036a15d5c7278db70c0ee29e84f5cca5b770
Steve Frost <ffrostt@gmail.com>
2016-12-28 15:04:44 -0600

    created six states for the six planned views

--- app/assets/javascripts/app.module.js
+++ app/assets/javascripts/app.module.js
@@ -4,7 +4,27 @@
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'views/static/home.html',
+        templateUrl: 'views/home.html'
+      })
+      .state('home.profile', {
+        url: '/profile',
+        templateUrl: 'views/profile.html'
+      })
+      .state('home.timeline', {
+        url: '/timeline',
+        templateUrl: 'views/timeline.html'
+      })
+      .state('home.watchlist', {
+        url: '/watchlist',
+        templateUrl: 'views/watchlist.html'
+      })
+      .state('documentaries', {
+        url: '/documentaries',
+        templateUrl: 'views/documentaries.html'
+      })
+      .state('documentaries.details', {
+        url: '/:movie/details',
+        templateUrl: 'views/details.html'
       });
 
     $urlRouterProvider.otherwise('/');
