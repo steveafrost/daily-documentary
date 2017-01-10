9d79c1ef44f488f43bb57dd63f8799925de9a025
Steve Frost <ffrostt@gmail.com>
2017-01-09 22:02:44 -0500

    updated handleList function to include docUrl and wired up to 'watch' action in details view

--- app/assets/javascripts/factories/doc.factory.js
+++ app/assets/javascripts/factories/doc.factory.js
@@ -18,14 +18,15 @@
 
       function handleList(response) {
         var allDocs = response.data.data.children;
-        var docTitles = [];
+        var docList = [];
 
         for (var i = 0; i < allDocs.length; i++) {
           docTitle = allDocs[i].data.title.toString();
+          docUrl = allDocs[i].data.url.toString();
           docTitle = docTitle.substring(0, docTitle.indexOf('('));
-          if (docTitle !== "") docTitles.push({id: [i], title: docTitle});
+          if (docTitle !== "") docList.push({id: [i], title: docTitle, url: docUrl});
         }
-        return docTitles;
+        return docList;
       }
 
       function getDocDetails(docTitle) {
