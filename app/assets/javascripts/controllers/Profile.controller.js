23f8786bdf747257e276de055f5508040ddbf46d
Steve Frost <ffrostt@gmail.com>
2017-01-11 22:09:51 -0500

    created structure for profile factory and controller

---
+++ app/assets/javascripts/controllers/Profile.controller.js
@@ -0,0 +1,10 @@
+(function() {
+
+angular.module('app')
+       .controller('Profile', Profile);
+
+  function Profile() {
+
+  }
+
+}());
--- app/assets/javascripts/factories/profile.factory.js
+++ app/assets/javascripts/factories/profile.factory.js
@@ -11,6 +11,24 @@
         getWatchlist: getWatchlist
       };
 
+      function getTimeline($http) {
+        return $http.get('/api/documentaries')
+                    .then(handleTimeline);
+      }
+
+      function handleTimeline(response) {
+        console.log(response);
+      }
+
+      function getWatchlist($http) {
+        return $http.get('/api/documentaries')
+                    .then(handleWatchlist);
+      }
+
+      function handleWatchlist(response) {
+        console.log(response);
+      }
+
     }
 
 }());
