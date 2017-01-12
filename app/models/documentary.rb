cb7a7f693be4a9741afa1e39ff29a0a2fc0b237c
Steve Frost <ffrostt@gmail.com>
2017-01-11 21:57:56 -0500

    modified the find_or_create for docs to update attributes if they've changed

--- app/models/documentary.rb
+++ app/models/documentary.rb
@@ -1,6 +1,8 @@
 class Documentary < ApplicationRecord
 
   def self.find_or_create(attributes)
-    self.where(title: attributes[:title]).first || self.create(attributes)
+    doc = self.where(title: attributes[:title]).first || self.create(attributes)
+    doc.assign_attributes(attributes)
+    doc
   end
 end
