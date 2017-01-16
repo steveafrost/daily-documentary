2a3050a6063564ff9123adc2274e9a8abeb7425c
Steve Frost <ffrostt@gmail.com>
2017-01-16 11:56:21 -0500

    wrapped app.module.js inside IFFE to match other angular files. added 'use-strict' to all angular files

--- app/assets/javascripts/app.module.js
+++ app/assets/javascripts/app.module.js
@@ -1,41 +1,47 @@
- angular
-  .module('dailyDocumentary', ['ui.router', 'templates', 'ngMaterial', 'ngMdIcons'])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'home.html'
-      })
-      .state('documentaries', {
-        url: '/documentaries',
-        templateUrl: 'documentaries.html',
-        controller: 'DocIndexController',
-        controllerAs: 'vm'
-      })
-      .state('documentaries.details', {
-        url: '/:movie/details',
-        templateUrl: 'details.html'
-      })
-      .state('profile', {
-        url: '/profile',
-        templateUrl: 'profile.html',
-        controller: 'ProfileController',
-        controllerAs: 'vm'
-      })
-      .state('profile.timeline', {
-        url: '/timeline',
-        templateUrl: 'timeline.html'
-      })
-      .state('profile.watchlist', {
-        url: '/watchlist',
-        templateUrl: 'watchlist.html'
-      });
+(function() {
 
-    $urlRouterProvider.otherwise('/');
+  'use-strict';
 
-    $locationProvider.html5Mode(true);
+  angular
+    .module('dailyDocumentary', ['ui.router', 'templates', 'ngMaterial', 'ngMdIcons'])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'home.html'
+        })
+        .state('documentaries', {
+          url: '/documentaries',
+          templateUrl: 'documentaries.html',
+          controller: 'DocIndexController',
+          controllerAs: 'vm'
+        })
+        .state('documentaries.details', {
+          url: '/:movie/details',
+          templateUrl: 'details.html'
+        })
+        .state('profile', {
+          url: '/profile',
+          templateUrl: 'profile.html',
+          controller: 'ProfileController',
+          controllerAs: 'vm'
+        })
+        .state('profile.timeline', {
+          url: '/timeline',
+          templateUrl: 'timeline.html'
+        })
+        .state('profile.watchlist', {
+          url: '/watchlist',
+          templateUrl: 'watchlist.html'
+        });
 
-    $mdThemingProvider.theme('default')
-      .primaryPalette('deep-purple')
-      .accentPalette('deep-purple');
+      $urlRouterProvider.otherwise('/');
+
+      $locationProvider.html5Mode(true);
+
+      $mdThemingProvider.theme('default')
+        .primaryPalette('deep-purple')
+        .accentPalette('deep-purple');
   });
+
+}());
