50c0d6bd3f443b6ccc715921637f5a36600cd6b9
Steve Frost <ffrostt@gmail.com>
2017-01-17 21:33:48 -0500

    added tweet action to share button & tweetText to AppCtrl

--- app/assets/javascripts/controllers/App.controller.js
+++ app/assets/javascripts/controllers/App.controller.js
@@ -10,6 +10,7 @@
     vm.toggleNav = toggleNav;
     vm.happyChange = happyChange;
     vm.happyCount = 0;
+    vm.tweetText = tweetText;
 
     function happyChange() {
       vm.happyCount++;
@@ -17,6 +18,10 @@
 
     function toggleNav() {
       $mdSidenav('left').toggle();
+    }
+
+    function tweetText() {
+      return "https://twitter.com/intent/tweet?text=Daily%20Documentary%20helps%20you%20find,%20watch,%20and%20keep%20track%20of%20captivating%20documentaries%20-%20check%20it%20out%20@%20steveafrost.com";
     }
   }
 
