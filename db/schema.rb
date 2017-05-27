379c09ed014eaadeaa13165e6b70994df5a58c03
Steve Frost <hello@steveafrost.com>
2017-05-26 23:02:00 -0400

    updated ActiveRecord to use Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170112021122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "documentaries", force: :cascade do |t|
     t.string  "title"
     t.string  "image"
